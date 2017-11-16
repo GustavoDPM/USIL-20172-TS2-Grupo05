5172716e224bd43a9859a8c2b8243bd4c712a947
MiguelRc <miguel.rc610@gmail.com>
2017-11-16 12:39:52 -0500

    MIGUEL ROLDAN
    ACTUALIZADO EL MENU
    ACTUALIZADO EL ESTILO CSS

---
+++ header.php
@@ -0,0 +1,12 @@
+<header id="Logo">
+            <div id="izquierda">
+               <span>
+                   <img src="http://cds.neoauto.g3c.pe/neoauto3/img/neoauto-inverted.svg?20171012184922">
+               </span> 
+            </div>
+            <div id="derecha">
+                <span id="registrate"><a href='registrate.php'>Registrate</a></span> 
+                <span id="iniciar"> <a href='inicia.php'>Iniciar Sesión</a></span>
+                <span id="vende"><a href='vende.php'>Vende tu auto</a></span>
+            </div>
+</header>
