4125a504b9d5204c600500b0c2f8defeb59739da
ChristopherTorresa <criss_wod_184@hotmail.com>
2017-11-19 18:23:05 -0500

    Trabajo terminado (por dos)
    Faltaba la interface de la confirmacion

--- carcochita/confirmacion.php
+++ carcochita/confirmacion.php
@@ -1,17 +1,18 @@
+<?php 
+include "configuracion.php";
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
 		<title>Bienvenido</title>
-		<style>
-			h1{text-align: center;font-family: arial;}
-			p{text-align:center;font-family: arial;}
-			a{text-align:center; margin-left: 43%; text-decoration: none; font-family: arial;}
-		</style>
+		<link href="css/estilo.css" rel="stylesheet" />
+		
 	</head>
 	
 	<body>
+	<?php include 'plantillas/header.php' ?>
 	<h1>¡HECHO! Usted se ha registrado con éxito.</h1>
 	<p>Bienvenido a la comunidad CARCOCHITA</p>
-	<a href="index.php">Volver a la página principal</a>
+	<?php include 'plantillas/pie.php' ?>
 	</body>
 </html>
